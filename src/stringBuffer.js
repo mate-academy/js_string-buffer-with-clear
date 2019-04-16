19c02688a45e5eff34810cff1af86a235d367757
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-04-16 16:34:18 +0300

    solution

--- src/stringBuffer.js
+++ src/stringBuffer.js
@@ -29,7 +29,17 @@
  * @return {function}
  */
 function makeBuffer() {
-  // write code here
+  let result = '';
+  let currentBuffer = function(value) {
+    if (value !== undefined) {
+      result += value;
+    }
+    return result;
+  };
+  currentBuffer.clear = function() {
+    result = '';
+  };
+  return currentBuffer;
 }
 
 module.exports = makeBuffer;
