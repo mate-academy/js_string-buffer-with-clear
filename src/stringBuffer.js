67c478881a2d720eff8ec33bcaefeca9b6f5cc68
Yevhenii <DrozhzhynY>
2020-04-08 19:21:58 +0300

    solution

--- src/stringBuffer.js
+++ src/stringBuffer.js
@@ -29,7 +29,19 @@
  * @return {function}
  */
 function makeBuffer() {
-  // write code here
+  let string = '';
+
+  function buffer(value = '') {
+    string += value;
+
+    return string;
+  }
+
+  buffer.clear = function() {
+    string = '';
+  };
+
+  return buffer;
 }
 
 module.exports = makeBuffer;
