81896ab72bc00048d51c01c439835ee4e3f55bb4
Denys <gryshko_denys@gmail.com>
2020-06-04 21:32:28 +0300

    solution

--- src/stringBuffer.js
+++ src/stringBuffer.js
@@ -29,7 +29,19 @@
  * @return {function}
  */
 function makeBuffer() {
-  // write code here
-}
+  let expression = '';
+
+  function buffer(str = '') {
+    expression += str;
+
+    return expression;
+  };
+
+  buffer.clear = function() {
+    expression = '';
+  };
+
+  return buffer;
+};
 
 module.exports = makeBuffer;
